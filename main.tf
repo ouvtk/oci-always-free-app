6c885cf09e9120b4e131e76838651b717b095911
Stepan Tsymbal <ouvtk@users.noreply.github.com>
2023-08-15 00:24:17 +1000

    Add placeholders for network resources

--- main.tf
+++ main.tf
@@ -20,6 +20,10 @@
 // Alerting, logging and monitoring.
 // Terraform state management by OCI.
 
+locals {
+  project = "app"
+}
+
 data "oci_identity_compartment" "parent" {
   id = var.parent_compartment_id
 }
@@ -43,11 +47,52 @@ data "oci_identity_compartment" "app" {
 resource "oci_core_vcn" "app" {
   compartment_id = data.oci_identity_compartment.app.id
 
-  cidr_blocks = []
+  cidr_blocks = ["10.0.0.0/16"]
 }
-
-
 
 data "oci_core_vcn" "app" {
   vcn_id = oci_core_vcn.app.id
+}
+
+resource "oci_core_internet_gateway" "internet_gw" {
+  compartment_id = data.oci_identity_compartment.app.id
+  vcn_id         = data.oci_core_vcn.app.id
+
+  display_name = local.project
+}
+
+// TODO: What to do with subnets if existing VCN is used?
+resource "oci_core_subnet" "app_load_balancer" {
+  compartment_id = data.oci_identity_compartment.app.id
+  vcn_id         = data.oci_core_vcn.app.id
+
+  cidr_block   = "10.0.1.0/24"
+  display_name = "${local.project}-lb"
+}
+
+// Instances subnet is meant to be private, but Always Free tier doesn't allow any NAT, only Internet Gateways.
+resource "oci_core_subnet" "app_compute" {
+  compartment_id = data.oci_identity_compartment.app.id
+  vcn_id         = data.oci_core_vcn.app.id
+
+  cidr_block   = "10.0.2.0/24"
+  display_name = "${local.project}-compute"
+}
+
+resource "oci_core_route_table" "app_load_balancer" {
+  compartment_id = data.oci_identity_compartment.app.id
+  vcn_id         = data.oci_core_vcn.app.id
+
+  route_rules {
+
+  }
+}
+
+resource "oci_core_route_table" "app_compute" {
+  compartment_id = data.oci_identity_compartment.app.id
+  vcn_id         = data.oci_core_vcn.app.id
+
+  route_rules {
+
+  }
 }
